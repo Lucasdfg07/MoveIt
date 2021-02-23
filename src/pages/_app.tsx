73ed5cd9f04442ae5563fe9109c6c8931d9ddb04
Lucas Siqueira <lucassiqueirafernandes07@gmail.com>
2021-02-23 15:54:54 -0300

    Setting Countdown component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
+import Head from 'next/head';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon.png"/>
+      </Head>
+
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import CompletedChallenges from '../components/CompletedChallenges';
+import Countdown from '../components/Countdown';
 import ExperienceBar from "../components/ExperienceBar";
 import Profile from '../components/Profile';
 
@@ -11,6 +12,7 @@ export default function Home() {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+        <title>Início | Move.it</title>
       </Head>
 
       <ExperienceBar />
@@ -19,6 +21,7 @@ export default function Home() {
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
 
         <div>
